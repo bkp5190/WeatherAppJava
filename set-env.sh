68c74d6450bd12928022f3cb5a86c2c35a97908b
Boney Patel <boneypatel37@yahoo.com>
2023-08-28 22:52:47 -0400

    fix env script

--- set-env.sh
+++ set-env.sh
@@ -1,4 +1,4 @@
 export OPENWEATHERMAP_URL=https://api.openweathermap.org
 # Make sure to fill in OPENWEATHERMAP_API_KEY and ZIP_CODE
-export OPENWEATHERMAP_API_KEY=${{ secrets.OPENWEATHERMAP_API_KEY }}
-export ZIP_CODE=${{ secrets.ZIP_CODE}}
+export OPENWEATHERMAP_API_KEY=$OPENWEATHERMAP_API_KEY
+export ZIP_CODE=$ZIP_CODE
